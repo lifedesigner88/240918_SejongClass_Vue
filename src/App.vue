657e0a6292cb9b536bbef78df2d97ae291bd9d6b
SejongPark <lifedesigner88@gmail.com>
2024-08-30 18:00:15 +0900

    FireBase!!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,46 @@
 <script setup>
 
 import {ref} from 'vue'
+import {firebaseApp} from "@/firebase";
+
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+
+// ✅ 로그인
+const auth = getAuth(firebaseApp);
+
+const loginInfo = ref({
+  txtEmail: '',
+  txtPassword: '',
+});
+
+const loginEmailPassword = async () => {
+
+  const loginEmail = loginInfo.value.txtEmail;
+  const loginPassword = loginInfo.value.txtPassword;
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+    );
+
+    console.log(userCredential.user);
+  } catch (error) {
+    if(error.code === "auth/invalid-email")
+      console.log("잘못된 이메일 입니다.")
+    else if (error.code === "auth/invalid-credential")
+      console.log("비밀번호가 틀렸습니다.")
+    else if (error.code === "auth/missing-password")
+      console.log("비밀번호를 입력해주세요.")
+
+  }
+}
+
 
 const BASEURL = 'https://port-0-java-springboottest-m0ce3xjwfbac249f.sel4.cloudtype.app'
 // const BASEURL = 'http://localhost:8080'
@@ -19,7 +59,6 @@ const changeh1Id = () => {
 }
 
 const testData = ref("")
-
 const getUsers = async () => {
   const response = await fetch(`${BASEURL}/user`, {
     method: 'GET',
@@ -30,18 +69,18 @@ const getUsers = async () => {
   testData.value = await response.json();
   console.log(testData.value);
 }
-
 const postUserName = async () => {
   await fetch(`${BASEURL}/user`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify( {name : name.value} )
+    body: JSON.stringify({name: name.value})
   });
   testData.value = `${name.value} "입력 성공"`;
   console.log(testData.value);
 }
+
 
 </script>
 
@@ -63,9 +102,13 @@ const postUserName = async () => {
 
 
     <button @click="postUserName">Post User</button>
-    <input v-model="name" placeholder="추가할 이름을 입력하세요" />
+    <input v-model="name" placeholder="추가할 이름을 입력하세요"/>
 
 
+    <h1>로그인 테스트</h1>
+    <input v-model="loginInfo.txtEmail" placeholder="이메일 입력해주세요"/><br>
+    <input v-model="loginInfo.txtPassword" placeholder="패스워드 입력해주세요."/>
+    <button @click="loginEmailPassword">로그인</button>
 
   </div>
 
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+
+export const firebaseApp = initializeApp({
+    apiKey: "AIzaSyDYhE8Gb1Y7X3ej9UELUMM39PstUcAT_kA",
+    authDomain: "fir-test-aa863.firebaseapp.com",
+    projectId: "fir-test-aa863",
+    storageBucket: "fir-test-aa863.appspot.com",
+    messagingSenderId: "657086984104",
+    appId: "1:657086984104:web:6fa4c3c9d20e301561779d",
+    measurementId: "G-NWBG5MH281"
+});
